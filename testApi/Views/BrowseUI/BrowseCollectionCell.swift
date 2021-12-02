f4e4c3e8422b24ea154abd6279a9ae65b8e3da88
Oleg Bezrukavnikov <olegbezr@berkeley.edu>
2021-12-01 16:45:44 -0800

    Added likes and ranking to RecipeCell

--- testApi/Views/BrowseUI/BrowseCollectionCell.swift
+++ testApi/Views/BrowseUI/BrowseCollectionCell.swift
@@ -25,6 +25,8 @@ class BrowseCollectionCell: UICollectionViewCell {
                 }
             })
             titleView.text = recipe?.title
+            likesView.text = String(recipe?.likes ?? 0)
+            ratingView.text = String(recipe?.rating ?? 0)
         }
     }
     
@@ -46,6 +48,44 @@ class BrowseCollectionCell: UICollectionViewCell {
         return label
     }()
     
+    private let likesImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "heart")?.withTintColor(.red, renderingMode: .alwaysOriginal)
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private let likesView: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .medium)
+        label.textColor = .label
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let ratingImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "star")?.withTintColor(.orange, renderingMode: .alwaysOriginal)
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private let ratingView: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .medium)
+        label.textColor = .label
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -54,6 +94,10 @@ class BrowseCollectionCell: UICollectionViewCell {
         
         contentView.addSubview(imageView)
         contentView.addSubview(titleView)
+        contentView.addSubview(likesView)
+        contentView.addSubview(ratingView)
+        contentView.addSubview(likesImageView)
+        contentView.addSubview(ratingImageView)
         
         NSLayoutConstraint.activate([
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
@@ -62,7 +106,18 @@ class BrowseCollectionCell: UICollectionViewCell {
             imageView.bottomAnchor.constraint(equalTo: titleView.topAnchor, constant: -7),
             titleView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             titleView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            titleView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            titleView.bottomAnchor.constraint(equalTo: likesView.topAnchor, constant: -7),
+            likesView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            likesImageView.topAnchor.constraint(equalTo: likesView.topAnchor),
+            likesImageView.bottomAnchor.constraint(equalTo: likesView.bottomAnchor),
+            ratingImageView.topAnchor.constraint(equalTo: likesView.topAnchor),
+            ratingImageView.bottomAnchor.constraint(equalTo: likesView.bottomAnchor),
+            likesImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            likesView.leadingAnchor.constraint(equalTo: likesImageView.trailingAnchor, constant: 3),
+            ratingImageView.leadingAnchor.constraint(equalTo: likesView.trailingAnchor, constant: 5),
+            ratingView.leadingAnchor.constraint(equalTo: ratingImageView.trailingAnchor, constant: 3),
+            ratingView.topAnchor.constraint(equalTo: likesView.topAnchor),
+            ratingView.bottomAnchor.constraint(equalTo: likesView.bottomAnchor)
         ])
     }
     
